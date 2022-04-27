1c072ca6758bae46d362f69a4499d9245bf2cfce
wilhsu1 <100149599+wilhsu1@users.noreply.github.com>
2022-04-27 15:52:02 -0400

    Update player3.py

--- Code/player3.py
+++ Code/player3.py
@@ -1,67 +1,114 @@
 import pygame
 from settings import *
-from tile import Tile
-from player3 import Player3
-from debug import debug
+from support import import_folder
 
+class Player3(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, obstacle_sprites):
+        super().__init__(groups)
+        self.image = pygame.image.load("../graphics/test/knight1.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0,-20)
 
-class Level3:  # Contain all the sprites + interactions
-    def __init__(self):
+        #Graphics setup
+        self.import_player_assets()
+        self.status = "down"
+        self.frame_index = 0
+        self.animation_speed = .15
 
-        # get the display surface
-        self.display_surface = pygame.display.get_surface()
+        self.direction = pygame.math.Vector2()  # Vector that is going to have x and y
+        self.speed = 5
 
-        # sprite group setup
-        self.visible_sprites = YSortCameraGroup()  # Will be drawin in screen (Player/Map/Obstacle)
-        self.obstacle_sprites = pygame.sprite.Group()  # Can collide with player
+        self.obstacle_sprites = obstacle_sprites
 
-        # sprite setup
-        self.create_map()
+    def import_player_assets(self):
+        character_path = "../graphics/test/rogue/" 
+        self.animations = {
+            "up": [],
+            "down": [],
+            "left": [],
+            "right": [],
+            "right_idle": [],
+            "left_idle": [],
+            "up_idle": [],
+            "down_idle": [],
+        } #All animation states
 
-    def create_map(self):
-        for row_index, row in enumerate(GAME_MAP):  # Each row, know the index
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE  # X position
-                y = row_index * TILESIZE  # Y position
-                if col == "x":  # Set up a certain kind of sprite
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                if col == "p":
-                    self.player3 = Player3(
-                        (x, y), [self.visible_sprites], self.obstacle_sprites
-                    )
-           
-    def run(self):
-        # update and draw the game
-        self.visible_sprites.custom_draw(self.player3)  # Display visible sprites
-        self.visible_sprites.update()
-        # debug(self.player.direction) #Displays the [x,y] of the player
-        # debug(self.player.status) #Displays the status of the player
+        for animation in self.animations.keys():
+            # print(animation)
+            full_path = character_path + animation #Create path
+            self.animations[animation] = import_folder(full_path) #Import images
 
+    def input(self):
+        keys = pygame.key.get_pressed()
 
+        # Movement Input
+        if keys[pygame.K_UP]:
+            self.direction.y = -1  # Going up
+            self.status = "up"
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1  # Going down
+            self.status = "down"
+        else:
+            self.direction.y = (0) # IF neither up or down key being pressed player does not move
 
-class YSortCameraGroup(pygame.sprite.Group):
-    # Sort the sprite by Y-coordinate
-    # Make a camera
-    def __init__(self):
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+            self.status = "right"
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+            self.status = "left"
+        else:
+            self.direction.x = 0
 
-        # general setup
-        super().__init__()
-        self.display_surface = pygame.display.get_surface() # Display the surface
-        self.half_width = (
-            self.display_surface.get_size()[0] // 2
-        )  # Player in the middle --> Distance from the left
-        self.half_height = (
-            self.display_surface.get_size()[1] // 2
-        )  # Player in the middle --> Distance from the top
-        self.offset = pygame.math.Vector2()
+    def get_status(self):
+        # idle status
+        if self.direction.x == 0 and self.direction.y == 0:  # Check if direction is 0
+            if not "idle" in self.status:  # check if status does not contain idle/ attacking
+                self.status = (self.status + "_idle")  # Only add idle once and not multiple times
 
-    def custom_draw(self, player3):
+    def move(self, speed):  # Update + draw the game
+        if self.direction.magnitude() != 0:  # Vector of 0 can not be normalized
+            self.direction = self.direction.normalize()  # Vector length = 1
 
-        # getting the offset of the player
-        self.offset.x = player3.rect.centerx - self.half_width  #
-        self.offset.y = player3.rect.centery - self.half_height  #
+        self.hitbox.x += self.direction.x * speed
+        self.collision("horizontal")  # Horizontal Collisions
+        self.hitbox.y += self.direction.y * speed
+        self.collision("vertical")  # Vertical Collisions
+        self.rect.center = self.hitbox.center
 
-        for sprite in sorted(self.sprites(), key = lambda sprite:sprite.rect.centery):
-            offset_pos = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image, offset_pos)
+    def collision(self, direction):  # Check for collisions
+        if direction == "horizontal":
+            for sprite in self.obstacle_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.x > 0:  # Moving right
+                        self.hitbox.right = sprite.hitbox.left
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.x < 0:  # Moving left
+                        self.hitbox.left = sprite.hitbox.right
 
+        if direction == "vertical":
+            for sprite in self.obstacle_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.y > 0:  # Moving down
+                        self.hitbox.bottom = sprite.hitbox.top
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.y < 0:  # Moving up
+                        self.hitbox.top = sprite.hitbox.bottom
+
+    def animate(self):
+        animation = self.animations[self.status]
+
+        # loop over the frame index
+        self.frame_index += self.animation_speed # Continuously larger number
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        # Set the image
+        self.image = animation[int(self.frame_index)] #Convert frame index to integer
+        self.rect = self.image.get_rect(center = self.hitbox.center)
+ 
+    def update(self):
+        self.input()
+        self.get_status()
+        self.animate()
+        self.move(self.speed)
