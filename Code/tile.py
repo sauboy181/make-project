09ed7397779f3662bfaf9c713b3b351432d97343
sauboy181 <103335223+sauboy181@users.noreply.github.com>
2022-05-01 16:09:16 -0400

    Update tile.py

--- Code/tile.py
+++ Code/tile.py
@@ -11,7 +11,7 @@ class Tile(pygame.sprite.Sprite):
 class Key(pygame.sprite.Sprite):
 	def __init__(self,pos,groups):
 		super().__init__(groups)
-		self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
+		self.image = pygame.image.load('../graphics/test/deadpool.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(0,-10) # Changes size of rectangle --> - 10 shrink 5 pixel on each side
 
