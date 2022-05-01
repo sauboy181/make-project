e46fa26d943fa36ad509e0dfb68a32cf0b10bc6d
sauboy181 <103335223+sauboy181@users.noreply.github.com>
2022-05-01 16:02:29 -0400

    Update tile.py
    added a new sprite file

--- Code/tile.py
+++ Code/tile.py
@@ -7,4 +7,11 @@ class Tile(pygame.sprite.Sprite):
 		self.image = pygame.image.load('../graphics/test/tree.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(0,-10) # Changes size of rectangle --> - 10 shrink 5 pixel on each side
+		
+class Key(pygame.sprite.Sprite):
+	def __init__(self,pos,groups):
+		super().__init__(groups)
+		self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0,-10) # Changes size of rectangle --> - 10 shrink 5 pixel on each side
 
