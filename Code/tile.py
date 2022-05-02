0ed2f308530fb6d43b56c5131b1dfb6e4b213c04
wilhsu1 <100149599+wilhsu1@users.noreply.github.com>
2022-05-01 20:37:18 -0400

    Updated tiles

--- Code/tile.py
+++ Code/tile.py
@@ -15,6 +15,34 @@ class Tile1(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(0,-10) # Changes size of rectangle --> - 10 shrink 5 pixel on each side
 
+class Tile2(pygame.sprite.Sprite):
+	def __init__(self,pos,groups):
+		super().__init__(groups)
+		self.image = pygame.image.load('../graphics/test/house1.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0,-10) # Changes size of rectangle --> - 10 shrink 5 pixel on each side
+
+class Tile3(pygame.sprite.Sprite):
+	def __init__(self,pos,groups):
+		super().__init__(groups)
+		self.image = pygame.image.load('../graphics/test/house2.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0,-10) # Changes size of rectangle --> - 10 shrink 5 pixel on each side
+
+class Tile4(pygame.sprite.Sprite):
+	def __init__(self,pos,groups):
+		super().__init__(groups)
+		self.image = pygame.image.load('../graphics/test/house3.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0,-10) # Changes size of rectangle --> - 10 shrink 5 pixel on each side
+
+class Tile5(pygame.sprite.Sprite):
+	def __init__(self,pos,groups):
+		super().__init__(groups)
+		self.image = pygame.image.load('../graphics/test/house4.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0,-10) # Changes size of rectangle --> - 10 shrink 5 pixel on each side
+
 class Key(pygame.sprite.Sprite):
 	def __init__(self,pos,groups):
 		super().__init__(groups)
